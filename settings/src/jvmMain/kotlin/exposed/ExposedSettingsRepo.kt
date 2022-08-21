f943daf1ab4d677598d5a2d5de3ea7ff6c73b161
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-08-21 18:25:51 +0600

    add cacheable settings

--- settings/src/jvmMain/kotlin/exposed/ExposedSettingsRepo.kt
+++ settings/src/jvmMain/kotlin/exposed/ExposedSettingsRepo.kt
@@ -1,10 +1,15 @@
 package dev.inmo.plaguposter.settings.exposed
 
 import dev.inmo.micro_utils.repos.KeyValueRepo
+import dev.inmo.micro_utils.repos.MapKeyValueRepo
+import dev.inmo.micro_utils.repos.cache.cache.FullKVCache
+import dev.inmo.micro_utils.repos.cache.full.FullKeyValueCacheRepo
+import dev.inmo.micro_utils.repos.cache.full.cached
 import dev.inmo.micro_utils.repos.exposed.keyvalue.ExposedKeyValueRepo
 import dev.inmo.micro_utils.repos.mappers.withMapper
 import dev.inmo.plaguposter.settings.repo.SettingsRepo
 import dev.inmo.tgbotapi.types.ChatId
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
@@ -45,6 +50,27 @@ inline fun <reified T : Any> ExposedSettingsRepo(
 )
 
 @OptIn(InternalSerializationApi::class)
+inline fun <reified T : Any> CachedExposedSettingsRepo(
+    database: Database,
+    json: Json,
+    scope: CoroutineScope,
+    serializer: KSerializer<T> = T::class.serializer(),
+    tableName: String = "settings_${T::class.simpleName!!}"
+) = ExposedSettingsRepo<T>(
+    ExposedKeyValueRepo(
+        database,
+        { long("chat_id") },
+        { text("settings") },
+        tableName
+    ).cached(
+        FullKVCache(MapKeyValueRepo()),
+        scope
+    ),
+    json,
+    serializer
+)
+
+@OptIn(InternalSerializationApi::class)
 inline fun <reified T : Any> SettingsRepo(
     parent: KeyValueRepo<Long, String>,
     json: Json,
@@ -58,3 +84,16 @@ inline fun <reified T : Any> SettingsRepo(
     serializer: KSerializer<T> = T::class.serializer(),
     tableName: String = "settings_${T::class.simpleName!!}"
 ) = ExposedSettingsRepo(database, json, serializer, tableName)
+
+
+
+@OptIn(InternalSerializationApi::class)
+inline fun <reified T : Any> CachedSettingsRepo(
+    database: Database,
+    json: Json,
+    scope: CoroutineScope,
+    serializer: KSerializer<T> = T::class.serializer(),
+    tableName: String = "settings_${T::class.simpleName!!}"
+) = CachedExposedSettingsRepo(database, json, scope, serializer, tableName)
+
+

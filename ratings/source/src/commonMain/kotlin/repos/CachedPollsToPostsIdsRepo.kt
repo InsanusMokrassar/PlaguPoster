54eb7515d3dce37fd800e70757eec4285445fe03
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-08-13 00:02:19 +0600

    small fixes in repos

--- ratings/source/src/commonMain/kotlin/repos/CachedPollsToPostsIdsRepo.kt
+++ ratings/source/src/commonMain/kotlin/repos/CachedPollsToPostsIdsRepo.kt
@@ -2,8 +2,7 @@ package dev.inmo.plaguposter.ratings.source.repos
 
 import dev.inmo.micro_utils.repos.KeyValueRepo
 import dev.inmo.micro_utils.repos.cache.cache.FullKVCache
-import dev.inmo.micro_utils.repos.cache.full.cached
-import dev.inmo.plaguposter.common.ShortMessageInfo
+import dev.inmo.micro_utils.repos.cache.full.fullyCached
 import dev.inmo.plaguposter.posts.models.PostId
 import dev.inmo.tgbotapi.types.PollIdentifier
 import kotlinx.coroutines.CoroutineScope
@@ -12,4 +11,4 @@ class CachedPollsToPostsIdsRepo(
     private val repo: PollsToPostsIdsRepo,
     private val scope: CoroutineScope,
     private val kvCache: FullKVCache<PollIdentifier, PostId> = FullKVCache()
-) : PollsToPostsIdsRepo, KeyValueRepo<PollIdentifier, PostId> by repo.cached(kvCache, scope)
+) : PollsToPostsIdsRepo, KeyValueRepo<PollIdentifier, PostId> by repo.fullyCached(kvCache, scope)
